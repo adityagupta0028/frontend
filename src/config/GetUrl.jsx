daf93ac929cc717f73250c6e4705a96217febaf0
mdali8630 <mohammadali10298@gmail.com>
2025-12-18 23:21:37 +0530

    upload code

--- src/config/GetUrl.jsx
+++ src/config/GetUrl.jsx
@@ -1,7 +1,4 @@
 export const GetUrl = {
-  API_URL:process.env.VITE_API_URL,
-  IMAGE_URL:process.env.VITE_IMAGE_URL,
-  //REACT_APP_MERCHANDISE_ID:process.env.REACT_APP_MERCHANDISE_ID
+  API_URL: import.meta.env.VITE_API_URL,
+  IMAGE_URL: import.meta.env.VITE_IMAGE_URL,
 };
-
-console.log("API URL:", GetUrl.API_URL);
