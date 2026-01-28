019486a8e9bcc2c9e7dd70f28407e9bdb7cd0c73
mdali8630 <mohammadali10298@gmail.com>
2026-01-28 23:01:41 +0530

    upload

--- src/config/GetUrl.jsx
+++ src/config/GetUrl.jsx
@@ -1,5 +1,5 @@
 export const GetUrl = {
   API_URL: import.meta.env.VITE_API_URL,
-  //  IMAGE_URL: 'https://api.merefunds.in',
-  IMAGE_URL:"http://localhost:8081"
+    IMAGE_URL: 'https://api.merefunds.in',
+  // IMAGE_URL:"http://localhost:8081"
 };
